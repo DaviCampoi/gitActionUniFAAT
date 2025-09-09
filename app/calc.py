69f697192b8c98df2425283a68a1e9ec9e7c5825
Davi Campoi <141076821+DaviCampoi@users.noreply.github.com>
2025-09-08 23:55:16 -0300

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,6 +1,26 @@
 def soma(a, b):
-    # return a - b  # ERRO proposital
-    return a + b  # Correção
+    return a + b
+
+def subtrai(a, b):
+    return a - b
 
 def multiplica(a, b):
     return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Não é possível dividir por zero.")
+    return a / b
+
+def potencia(a, b):
+    return a ** b
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("Soma:", soma(a, b))
+    print("Subtração:", subtrai(a, b))
+    print("Multiplicação:", multiplica(a, b))
+    print("Divisão:", divide(a, b))
+    print("Potenciação:", potencia(a, b))
