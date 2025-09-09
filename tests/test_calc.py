3cd3aa5aa88ccced43e99265ab2a99472f045296
Davi Campoi <141076821+DaviCampoi@users.noreply.github.com>
2025-09-08 23:42:09 -0300

    Update test_calc.py

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,19 @@
-from app.calc import soma, multiplica
+from app.calc import soma, subtrai, multiplica, divide, potencia
+import pytest
 
 def test_soma():
     assert soma(2, 3) == 5
 
+def test_subtrai():
+    assert subtrai(5, 3) == 2
+
 def test_multiplica():
     assert multiplica(2, 3) == 6
+
+def test_divide():
+    assert divide(10, 2) == 5
+    with pytest.raises(ValueError):
+        divide(10, 0)
+
+def test_potencia():
+    assert potencia(2, 3) == 8
